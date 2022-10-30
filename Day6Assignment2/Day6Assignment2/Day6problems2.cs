563e2067a651d66ff1a5c6c01fc2004d71d7aee5
sanjeev <sanjeevsingh.77d@gmail.com>
2022-10-30 10:47:14 +0530

    Vending Machine

---
+++ Day6Assignment2/Day6Assignment2/Day6problems2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day6Assignment2
+{
+    internal class Day6problems2
+    {
+
+        public  void RupeesNotes(int amount)
+        {
+            int[] currency = new int[] { 1, 2, 5, 10, 50,100, 500,1000 };
+            int[] currencyCounter = new int[10];
+
+            for (int i = 0; i < 10; i++)
+            {
+                if ( amount >= currency[i] )
+                {
+                    currencyCounter[i] = amount / currency[i];
+                    amount = amount % currency[i];
+                }
+            }
+
+            Console.WriteLine("Currency Count: ");
+            for (int i = 0; i < 10; i++)
+            {
+                if (currencyCounter[i] != 0)
+                {
+                    Console.WriteLine(currency[i] + " : " + currencyCounter[i]);
+                }
+            }
+        }
+    }
+}
---
+++ Day6Assignment2/Day6Assignment2/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Day6Assignment2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Day6problems2 obj = new Day6problems2();
+            Console.Write("enter amount : ");
+            int amount = Convert.ToInt32(Console.ReadLine());
+            obj.RupeesNotes(amount);
+        }
+
+
+    }
+}
